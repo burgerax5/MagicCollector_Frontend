cd6d9bfbcc5db6fe547e12d474c4239352364d1d
burgerax5 <burgerax3@hotmail.com>
2024-06-29 14:37:09 +1200

    Creating the navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import LogoDark from '../../assets/logo_dark.png'
+import { Link } from "react-router-dom"
+import '../../styles/header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <img src={LogoDark} />
+        <nav className="navbar">
+            <Link to="/" className="logo">
+                <img src={LogoDark} />
+            </Link>
+
+            <ul>
+                <li>
+                    <Link to="/cards">All Cards</Link>
+                </li>
+                <li>
+                    <Link to="/sets">All Sets</Link>
+                </li>
+                <li>
+                    <Link to="/mycards">My Cards</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
