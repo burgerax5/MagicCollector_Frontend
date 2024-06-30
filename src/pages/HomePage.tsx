553b5b6cb22963172e5ec42ed9284b162732dc76
burgerax5 <burgerax3@hotmail.com>
2024-06-30 16:31:36 +1200

    Added hero section to home page
    - Changed website name to MagicGatherer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import LogoDark from '../assets/logo_dark.png';
+import LogoLight from '../assets/logo_light.png';
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/reducers/rootReducer'
+import { Link } from 'react-router-dom';
+import '../styles/home.css'
 
 const HomePage = () => {
+    const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
+
     return (
-        <div>HomePage</div>
+        <div>
+            <section className="hero">
+                <div className="hero-content">
+                    <img src={isDarkMode ? LogoDark : LogoLight} />
+                    <p>Track the prices of your Magic: The Gathering card collection</p>
+                    <Link to="/register" className="call-to-action">Sign Up Today!</Link>
+                </div>
+                <div className="hero-gradient"></div>
+                <div className="hero-bg"></div>
+            </section>
+        </div>
     )
 }
 
