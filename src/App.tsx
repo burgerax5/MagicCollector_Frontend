5302aa4f8837856d62a99c749184c65313bf4928
burgerax5 <burgerax3@hotmail.com>
2024-06-30 20:39:03 +1200

    Added article section to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Header from './components/Header/Header'
 import { Outlet } from "react-router-dom"
 import './styles/main.css'
 
 function App() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Outlet />
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,14 @@ import { RootState } from '../redux/reducers/rootReducer'
 import { Link } from 'react-router-dom';
 import '../styles/home.css'
 
+import TempImg from '../assets/tmp_img.png'
+import TempImg2 from '../assets/tmp_img2.png'
+
 const HomePage = () => {
     const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
 
     return (
-        <div>
+        <div style={{ background: "var(--background)" }}>
             <section className="hero">
                 <div className="hero-content">
                     <img src={isDarkMode ? LogoDark : LogoLight} />
@@ -18,6 +21,20 @@ const HomePage = () => {
                 </div>
                 <div className="hero-gradient"></div>
                 <div className="hero-bg"></div>
+            </section>
+            <section className="home-content">
+                <article>
+                    <h2>Track the Prices of <br></br>Your Collection</h2>
+                    <p>
+                        MagicGatherer will scrape data from CardKingdom every 24 hours so you can easily keep track of the prices of your cards
+                    </p>
+                    <ul>
+                        <li>Prices for different card conditions</li>
+                        <li>Variants (e.g. foils)</li>
+                        <li>Filters to make it easier to search for a particular card</li>
+                    </ul>
+                </article>
+                <img src={isDarkMode ? TempImg : TempImg2} />
             </section>
         </div>
     )
