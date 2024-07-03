de7abae044e0a14c05d6a3f2669e37ba513cf1ae
burgerax5 <burgerax3@hotmail.com>
2024-07-03 12:14:03 +1200

    Moved RouterProvider from main.tsx to App.tsx
    App.tsx had barely anything in it so I moved some of the code from main.tsx into it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import { Outlet } from "react-router-dom"
 import './styles/main.css'
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import Layout from './Layout';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UsersOnly from './components/UsersOnly'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><Outlet /></Layout>,
+    errorElement: <Layout><ErrorPage /></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        path: "login",
+        element: <LoginPage />
+      },
+      {
+        path: "register",
+        element: <RegisterPage />
+      },
+      {
+        path: "mycards",
+        element: <UsersOnly><h1>HI</h1></UsersOnly>
+      }
+    ]
+  },
+]);
 
 function App() {
   return (
-    <Outlet />
+    <RouterProvider router={router} />
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import Layout from './Layout';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout><App /></Layout>,
-    errorElement: <Layout><ErrorPage /></Layout>,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />
-      },
-      {
-        path: "login",
-        element: <LoginPage />
-      },
-      {
-        path: "register",
-        element: <RegisterPage />
-      }
-    ]
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
