bcd299450cdfeab1645971205307949dc55f4e1b
burgerax5 <burgerax3@hotmail.com>
2024-07-12 11:56:26 +1200

    fix: handle jwt expiration
    Also slight changes to the /cards page styling

--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -2,18 +2,43 @@ import { LoginActionType } from "../actions/actions"
 import { LOGIN, LOGOUT } from "../actions/actionTypes"
 import User from '../../models/User'
 import Cookies from "js-cookie"
+import { jwtDecode } from "jwt-decode"
 
 type AuthState = {
     isAuthenticated: boolean,
     user: User | null
 }
 
+type Payload = {
+    unique_name: string,
+    nbf: number,
+    exp: number,
+    iat: number
+}
+
+const verifyToken = (token: string): boolean => {
+    try {
+        const decoded: Payload = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+
+        // Check if token has expired
+        if (decoded.exp < currentTime) {
+            Cookies.remove("auth");
+            return false;
+        }
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 const authCookie = Cookies.get("auth");
-const hasAuthCookie = authCookie !== undefined;
+const hasAuthCookie = authCookie !== undefined && verifyToken(authCookie);
 
 const initialState: AuthState = {
     isAuthenticated: hasAuthCookie,
-    user: hasAuthCookie ? { username: authCookie as string } : null
+    user: hasAuthCookie ? { username: jwtDecode<Payload>(authCookie).unique_name } : null
 }
 
 const authReducer = (state: AuthState = initialState, action: LoginActionType) => {
