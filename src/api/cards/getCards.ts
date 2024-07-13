1c89f3834b41368057c1f402c535d09b22e7b11e
burgerax5 <burgerax3@hotmail.com>
2024-07-13 18:25:44 +1200

    style: update styling of foil cards again
    Also added a try catch block for the `getCardsInPage()` function

--- src/api/cards/getCards.ts
+++ src/api/cards/getCards.ts
@@ -2,9 +2,19 @@ import { CardPageDTO } from "../../models/Cards/CardPageDTO";
 
 const getCardsInPage = async (params: string) => {
     const url = 'https://localhost:44321/api/cards?' + params;
-    const page = await fetch(url)
-        .then(response => response.json()) as CardPageDTO;
-    return page;
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(`HTTP Error. Status: ${response.status}`);
+        }
+
+        const pageData = await response.json();
+        return pageData as CardPageDTO;
+    } catch (error) {
+        console.error('Error fetching cards:', error);
+        throw error;
+    }
+
 }
 
 export default getCardsInPage;
