67df03757e695902de1464663032adb419312775
burgerax5 <burgerax3@hotmail.com>
2024-07-24 13:26:20 +1200

    fix: change apiURL to azure web app

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
-// const apiURL = "https://magicgathererapi.azurewebsites.net";
-const apiURL = "https://localhost:44321"
+const apiURL = "https://magicgathererapi.azurewebsites.net";
+// const apiURL = "https://localhost:44321"
 export default apiURL;
